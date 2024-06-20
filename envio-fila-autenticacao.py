685381c35ffd12436956e0a75a5df45721d29ac1
rennanvasconcellos <46463461+rennanvasconcellos@users.noreply.github.com>
2024-06-19 23:51:56 -0300

    Add files via upload

---
+++ envio-fila-autenticacao.py
@@ -0,0 +1,20 @@
+import pymqi
+
+queue_manager = 'QM.01'
+channel = 'C.TESTE'
+host = '192.168.68.119'
+port = '1414'
+queue_name = 'QL.APP.B'
+message = 'Hello from Python!'
+conn_info = '%s(%s)' % (host, port)
+
+user = 'mqm-app-b'
+password = 'mqm-app-b'
+
+qmgr = pymqi.connect(queue_manager, channel, conn_info, user, password)
+
+queue = pymqi.Queue(qmgr, queue_name)
+queue.put(message)
+queue.close()
+
+qmgr.disconnect()
