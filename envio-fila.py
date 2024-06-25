89dea30280ebc09776dbaea04074879635b818dc
rennanvasconcellos <46463461+rennanvasconcellos@users.noreply.github.com>
2024-06-24 21:26:31 -0300

    Update envio-fila.py

--- envio-fila.py
+++ envio-fila.py
@@ -1,7 +1,7 @@
 import pymqi
 
 print("Starting putmsg")
-queue_manager = pymqi.connect('QM.01', 'C.TESTE', '192.168.68.119(1414)')
+queue_manager = pymqi.connect('QM.01', 'C.TESTE', 'localhost(1414)')
 q = pymqi.Queue(queue_manager, 'QL.TESTE')
 q.put('Hello from Python!')
 print("Putting 1 message into queue")
